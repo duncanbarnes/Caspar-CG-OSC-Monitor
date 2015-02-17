bbc23e1932235e64848b7b2d4b8685fa6cc14cfc
Duncan Barnes <github@duncanbarnes.net>
2015-02-17 18:13:16 +0000

    Initial Commit
    Allows viewing of OSC messages from Caspar CG (and probably other OSC
    clients), some basic filtering of Caspar CG messages is also available.

---
+++ Caspar CG OSC Monitor/formMain.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Below namepaces are required for OSC operation
+using System.Net; 
+using Bespoke.Common;
+using Bespoke.Common.Osc;
+
+namespace Caspar_CG_OSC_Monitor
+{
+    public partial class formMain : Form
+    {
+        /// <summary>
+        /// OSC Server instance
+        /// </summary>
+        private OscServer _OscServer = new OscServer(TransportType.Udp, IPAddress.Any, 6250);
+
+
+        public formMain()
+        {
+            InitializeComponent();
+            //Subscribe to events that the OSC server raises, Message and BundleReceived
+            //some messages from Caspar are sent as bundles/groups of messages, others as
+            //individual messages so we need to listen to both to get all data about what
+            //Caspar is doing
+            this._OscServer.MessageReceived += _OscServer_MessageReceived;
+            this._OscServer.BundleReceived += _OscServer_BundleReceived;
+
+        }
+        /// <summary>
+        /// Handle incoming bundles/groups of messages from the OSC server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void _OscServer_BundleReceived(object sender, OscBundleReceivedEventArgs e)
+        {
+            foreach (OscMessage m in e.Bundle.Messages)
+            {
+                FilterOscMessage(m);
+            }
+        }
+        /// <summary>
+        /// Handle incoming individual messages from the OSC Server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void _OscServer_MessageReceived(object sender, OscMessageReceivedEventArgs e)
+        {
+            FilterOscMessage(e.Message);
+        }
+        /// <summary>
+        /// Allows basic filtering of some OSC messages before printing the messages to screen
+        /// </summary>
+        /// <param name="m"></param>
+        private void FilterOscMessage(OscMessage m)
+        {
+            string d1 = m.Data[0].ToString();
+            string d2 = (m.Data.Count > 1) ? m.Data[1].ToString() : null;
+            string d3 = (m.Data.Count > 2) ? m.Data[2].ToString() : null;
+
+            string[] address = m.Address.ToString().Split('/');
+            if (!checkBoxFilterMixer.Checked && address[3] == "mixer") return;
+            if (!checkBoxFilterStage.Checked && address[3] == "stage") return;
+            if (!checkBoxFilterOutputConsume.Checked && address[3] == "output" && address[4] == "consume_time") return;
+
+
+            dataGridViewIncomingMessages.Invoke(new MethodInvoker(() => {
+                dataGridViewIncomingMessages.Rows.Insert(0, DateTime.Now.ToString("HH:mm:ss"), m.Address.ToString(),d1, d2, d3);
+            }));
+        }
+        /// <summary>
+        /// Button click event allowing us to start and stop the OSC Server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonStartStopServer_Click(object sender, EventArgs e)
+        {
+            if (this._OscServer.IsRunning) //Stop the server
+            {
+                this.numericUpDownOSCPort.Enabled = true;
+                this.buttonStartStopServer.Text = "Start Server";
+                this._OscServer.Stop();
+            }
+            else // Start the server
+            {
+                this.numericUpDownOSCPort.Enabled = false;
+                this.buttonStartStopServer.Text = "Stop Server";
+                this._OscServer.Start();
+            }
+        }
+    }
+}
---
+++ Caspar CG OSC Monitor/formmain.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Caspar_CG_OSC_Monitor
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainerToolbarLogs = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStartStopServer = new System.Windows.Forms.Button();
+            this.numericUpDownOSCPort = new System.Windows.Forms.NumericUpDown();
+            this.dataGridViewIncomingMessages = new System.Windows.Forms.DataGridView();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Data1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Data2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Data3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkBoxFilterMixer = new System.Windows.Forms.CheckBox();
+            this.checkBoxFilterOutputConsume = new System.Windows.Forms.CheckBox();
+            this.checkBoxFilterStage = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerToolbarLogs)).BeginInit();
+            this.splitContainerToolbarLogs.Panel1.SuspendLayout();
+            this.splitContainerToolbarLogs.Panel2.SuspendLayout();
+            this.splitContainerToolbarLogs.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOSCPort)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIncomingMessages)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainerToolbarLogs
+            // 
+            this.splitContainerToolbarLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerToolbarLogs.IsSplitterFixed = true;
+            this.splitContainerToolbarLogs.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerToolbarLogs.Name = "splitContainerToolbarLogs";
+            this.splitContainerToolbarLogs.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainerToolbarLogs.Panel1
+            // 
+            this.splitContainerToolbarLogs.Panel1.Controls.Add(this.checkBoxFilterStage);
+            this.splitContainerToolbarLogs.Panel1.Controls.Add(this.checkBoxFilterOutputConsume);
+            this.splitContainerToolbarLogs.Panel1.Controls.Add(this.checkBoxFilterMixer);
+            this.splitContainerToolbarLogs.Panel1.Controls.Add(this.label1);
+            this.splitContainerToolbarLogs.Panel1.Controls.Add(this.buttonStartStopServer);
+            this.splitContainerToolbarLogs.Panel1.Controls.Add(this.numericUpDownOSCPort);
+            // 
+            // splitContainerToolbarLogs.Panel2
+            // 
+            this.splitContainerToolbarLogs.Panel2.Controls.Add(this.dataGridViewIncomingMessages);
+            this.splitContainerToolbarLogs.Size = new System.Drawing.Size(1205, 469);
+            this.splitContainerToolbarLogs.SplitterDistance = 70;
+            this.splitContainerToolbarLogs.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Monitor Port:";
+            // 
+            // buttonStartStopServer
+            // 
+            this.buttonStartStopServer.Location = new System.Drawing.Point(294, 15);
+            this.buttonStartStopServer.Name = "buttonStartStopServer";
+            this.buttonStartStopServer.Size = new System.Drawing.Size(128, 42);
+            this.buttonStartStopServer.TabIndex = 1;
+            this.buttonStartStopServer.Text = "Start Server";
+            this.buttonStartStopServer.UseVisualStyleBackColor = true;
+            this.buttonStartStopServer.Click += new System.EventHandler(this.buttonStartStopServer_Click);
+            // 
+            // numericUpDownOSCPort
+            // 
+            this.numericUpDownOSCPort.Location = new System.Drawing.Point(138, 24);
+            this.numericUpDownOSCPort.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numericUpDownOSCPort.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownOSCPort.Name = "numericUpDownOSCPort";
+            this.numericUpDownOSCPort.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDownOSCPort.TabIndex = 0;
+            this.numericUpDownOSCPort.Value = new decimal(new int[] {
+            6250,
+            0,
+            0,
+            0});
+            // 
+            // dataGridViewIncomingMessages
+            // 
+            this.dataGridViewIncomingMessages.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewIncomingMessages.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Time,
+            this.Address,
+            this.Data1,
+            this.Data2,
+            this.Data3});
+            this.dataGridViewIncomingMessages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewIncomingMessages.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewIncomingMessages.Name = "dataGridViewIncomingMessages";
+            this.dataGridViewIncomingMessages.RowHeadersVisible = false;
+            this.dataGridViewIncomingMessages.RowTemplate.Height = 28;
+            this.dataGridViewIncomingMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridViewIncomingMessages.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewIncomingMessages.Size = new System.Drawing.Size(1205, 395);
+            this.dataGridViewIncomingMessages.TabIndex = 0;
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            // 
+            // Address
+            // 
+            this.Address.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Address.HeaderText = "OSC Message Address";
+            this.Address.Name = "Address";
+            // 
+            // Data1
+            // 
+            this.Data1.HeaderText = "Data 1";
+            this.Data1.Name = "Data1";
+            // 
+            // Data2
+            // 
+            this.Data2.HeaderText = "Data 2";
+            this.Data2.Name = "Data2";
+            // 
+            // Data3
+            // 
+            this.Data3.HeaderText = "Data 3";
+            this.Data3.Name = "Data3";
+            // 
+            // checkBoxFilterMixer
+            // 
+            this.checkBoxFilterMixer.AutoSize = true;
+            this.checkBoxFilterMixer.Location = new System.Drawing.Point(453, 12);
+            this.checkBoxFilterMixer.Name = "checkBoxFilterMixer";
+            this.checkBoxFilterMixer.Size = new System.Drawing.Size(156, 24);
+            this.checkBoxFilterMixer.TabIndex = 3;
+            this.checkBoxFilterMixer.Text = "/channel/*/mixer/*";
+            this.checkBoxFilterMixer.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFilterOutputConsume
+            // 
+            this.checkBoxFilterOutputConsume.AutoSize = true;
+            this.checkBoxFilterOutputConsume.Location = new System.Drawing.Point(453, 40);
+            this.checkBoxFilterOutputConsume.Name = "checkBoxFilterOutputConsume";
+            this.checkBoxFilterOutputConsume.Size = new System.Drawing.Size(263, 24);
+            this.checkBoxFilterOutputConsume.TabIndex = 4;
+            this.checkBoxFilterOutputConsume.Text = "/channel/*/output/consume_time";
+            this.checkBoxFilterOutputConsume.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFilterStage
+            // 
+            this.checkBoxFilterStage.AutoSize = true;
+            this.checkBoxFilterStage.Location = new System.Drawing.Point(755, 12);
+            this.checkBoxFilterStage.Name = "checkBoxFilterStage";
+            this.checkBoxFilterStage.Size = new System.Drawing.Size(159, 24);
+            this.checkBoxFilterStage.TabIndex = 5;
+            this.checkBoxFilterStage.Text = "/channel/*/stage/*";
+            this.checkBoxFilterStage.UseVisualStyleBackColor = true;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 469);
+            this.Controls.Add(this.splitContainerToolbarLogs);
+            this.Name = "formMain";
+            this.Text = "Caspar CG OSC Monitor";
+            this.splitContainerToolbarLogs.Panel1.ResumeLayout(false);
+            this.splitContainerToolbarLogs.Panel1.PerformLayout();
+            this.splitContainerToolbarLogs.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerToolbarLogs)).EndInit();
+            this.splitContainerToolbarLogs.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOSCPort)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIncomingMessages)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainerToolbarLogs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStartStopServer;
+        private System.Windows.Forms.NumericUpDown numericUpDownOSCPort;
+        private System.Windows.Forms.DataGridView dataGridViewIncomingMessages;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data3;
+        private System.Windows.Forms.CheckBox checkBoxFilterOutputConsume;
+        private System.Windows.Forms.CheckBox checkBoxFilterMixer;
+        private System.Windows.Forms.CheckBox checkBoxFilterStage;
+    }
+}
+
